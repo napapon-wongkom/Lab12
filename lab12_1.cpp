407aeb1d6df68a88b811d121e2005390e913dadb
napapon-wongkom <dot.wongkom@gmail.com>
2023-01-06 15:42:55 +0700

    lab12_1.cpp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,17 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<ctime>
+using namespace std;
+string grade[9] = {"A","B+","B","C+","C","D+","D","F","W"};
+
+int main(){
+    srand(time(0));
+    int ran = rand()%9;
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    string result = grade[ran];
+    cout << "You will get "<< result <<" in this 261102.";
+
+}
+
